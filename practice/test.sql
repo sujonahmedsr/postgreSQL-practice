a32ad7c27cf35239e3a3eca376c8f3d619081df8
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-03-20 19:43:48 +0600

    updatd

--- practice/test.sql
+++ practice/test.sql
@@ -1,7 +1,20 @@
 -- Active: 1742287443448@@127.0.0.1@5432@ph
-CREATE TABLE person(  
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
-);
+-- CREATE TABLE person(  
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(255)
+-- );
 
- SELECT * from person;
+SELECT * FROM "user";
+
+ALTER Table "user"
+ADD COLUMN email VARCHAR(20) DEFAULT 'sjn@gmail.com' not NULL
+
+ALTER Table "user"
+RENAME COLUMN age to user_age
+
+ALTER Table "user"
+    DROP COLUMN email;
+
+INSERT INTO "user" 
+    VALUES  
+    (9, 'Rakib1',20)
